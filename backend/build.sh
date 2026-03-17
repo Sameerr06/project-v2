39662f47c08fbd26dba1069ab7d595dd8edc969f
Sameerr06 <sameeribrahim61@gmail.com>
2026-03-17 10:23:38 +0530

    Auto-create Django superuser admin/admin123

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,20 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser \
+      --no-input \
+      --username "$DJANGO_SUPERUSER_USERNAME" \
+      --email "$DJANGO_SUPERUSER_EMAIL"
+fi
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username = 'admin';
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, 'admin@example.com', 'admin123')
+    print('Superuser created')
+"
